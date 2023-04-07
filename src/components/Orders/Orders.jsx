bc4ea7f270b68ed41a1baae873bc2dce51346297
nimairoy <nimairoy718@gmail.com>
2023-04-07 17:47:28 +0600

    added cart remove handler button

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const carts = useLoaderData();
-    // console.log(carts)
+    const savedCart = useLoaderData();
+
+    // useState
+    const [cart, setCart] = useState(savedCart)
+
+    // handle the cart remove hanlder function
+    const handleCartRemoveItem = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
+
     return (
         <div className='shop-container'>
             <div className="reviewItem-container">
                     {
-                        carts.map(product => <ReviewItem
+                        cart.map(product => <ReviewItem
                              key={product.id}
-                             product ={product}                          
+                             product ={product} 
+                             handleCartRemoveItem={handleCartRemoveItem}                         
                             ></ReviewItem>)
                     }
             </div>
             <div className="cart-container">
-                <Cart cart={carts} />
+                <Cart cart={cart} />
             </div>
         </div>
     );
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-const ReviewItem = ({product}) => {
-    console.log(product)
+
+const ReviewItem = ({product, handleCartRemoveItem}) => {
+    // console.log(product)
     const {id, img, name, quantity, price, shipping} = product;
     return (
         <div className='reviewItem-card'>
@@ -13,8 +16,10 @@ const ReviewItem = ({product}) => {
                 <p>Quantity: {quantity}</p>
                 <p>Shipping Charge: {shipping}</p>
             </div>
-            <div>
-            <button>Delete</button>
+            <div className='trash-btn-container'>
+                <button onClick={()=>handleCartRemoveItem(id)} className='trash-btn'>
+                 <FontAwesomeIcon icon={faTrashCan} />
+                </button>
             </div>
         </div>
     );
